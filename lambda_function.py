3157e66a5a861b94ac45a4b99e4aec1d1f4fad2c
Andrey Fellipe <andreydida@hotmail.com>
2024-09-12 16:27:30 -0300

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,29 @@
+import json
+import requests
+
+def lambda_handler(event, context):
+    pokemon_name = event['pokemon_name']
+    
+    # Requisição à PokeAPI
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}')
+    
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        
+        # Filtrar dados relevantes
+        filtered_data = {
+            "name": pokemon_data["name"],
+            "height": pokemon_data["height"],
+            "weight": pokemon_data["weight"],
+            "abilities": [ability["ability"]["name"] for ability in pokemon_data["abilities"]]
+        }
+        
+        return {
+            'statusCode': 200,
+            'pokemon_data': filtered_data
+        }
+    else:
+        return {
+            'statusCode': response.status_code,
+            'message': 'Pokemon not found'
+        }
